9d2850dcf90c702bbdd82e3adcd0de54fecfcfdc
Sidddarthhegde <cddarth015@gmail.com>
2014-08-23 20:58:11 -0700

    first cpp project
    test

---
+++ experiment/test/cpptest/test.cpp
@@ -0,0 +1,16 @@
+#include "test.h"
+
+namespace sidharth
+{
+
+test::test()
+{
+}
+
+test::~test()
+{
+}
+
+
+}
+
---
+++ experiment/test/cpptest/test.h
@@ -0,0 +1,17 @@
+#ifndef TEST_H
+#define TEST_H
+
+namespace sidharth
+{
+
+class test
+{
+public:
+	test();
+	~test();
+
+};
+
+}
+
+#endif // TEST_H
